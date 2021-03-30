47b0304fcc61fcf59a41b7971ac7e2096fe24d60
oguhpereira <oguhpereira@protonmail.com>
2021-03-30 09:26:26 -0300

    feat: improve script add template

---
+++ script/getfiles.py
@@ -0,0 +1,43 @@
+import requests
+
+
+contents = [
+    'javascript', 
+    'python',
+    'php',
+    'html-css', 
+    'wordpress',
+    'swift',
+    'java',
+    'rubyonrails',
+    'go',
+    'php',
+    'elixir',
+    'go',
+    'csharp'
+]
+
+## get template
+file = open('./template.md','r')
+template = file.read()
+
+
+## get content
+def getContent(name):
+    content = requests.get('https://raw.githubusercontent.com/learnbr/' + name + '/master/README.md')
+    return content
+
+## write content
+def writeContent(prefix, fileName, content):
+    file = open(prefix + '2021-01-01-'+ fileName +'.md','a')
+    file.write(template + content.text)
+    file.close()
+
+def mountPosts(postContents):
+    for post in postContents:
+        content = getContent(post)
+        fileName = post.replace('https://raw.githubusercontent.com/learnbr/','').replace('/master/README.md','')
+        writeContent('../_posts/', fileName, content)
+
+
+mountPosts(contents)
